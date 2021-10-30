6c24456558a7a9763957f11e1c89f005c2169a3c
thuongthuy1256 <93435377+thuongthuy1256@users.noreply.github.com>
2021-10-30 20:30:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Gui cho...",
-    introTitle: 'Người toi iu',
+    introTitle: 'Bạn Ngôn ml',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
